71c6a24775ef707f2570eae1e3ad326cdcd414be
anderson_souza75@live.com <anderson_souza75@live.com>
2023-09-16 14:41:22 -0300

    Renomando projeto

--- Web/DeltaRHWeb/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace DeltaRHWeb.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
