da2f6622b1951d5897f476f79ad1540c4f427663
Anderson Oliveira de Souza <anderson_souza75@live.com>
2023-09-27 17:59:59 -0300

    Update script-dados-ficticios.sql

--- script-dados-ficticios.sql
+++ script-dados-ficticios.sql
@@ -1,136 +1,175 @@
-USE BD_DELTA;
+USE DB_DELTA
 
--- Inserir mais 10 valores fictícios na tabela tbl_missaovisaovalores
-INSERT INTO dbo.tbl_missaovisaovalores (descricao)
+INSERT INTO [dbo].[tbl_missaovisaovalores] ([descricao])
 VALUES
-  ('Missão 11'),
-  ('Missão 12'),
-  ('Missão 13'),
-  ('Missão 14'),
-  ('Missão 15'),
-  ('Missão 16'),
-  ('Missão 17'),
-  ('Missão 18'),
-  ('Missão 19'),
-  ('Missão 20');
-
--- Inserir mais 10 valores fictícios na tabela tbl_politicadisciplinar
-INSERT INTO dbo.tbl_politicadisciplinar (descricao)
+  ('Missão 1'),
+  ('Missão 2'),
+  ('Missão 3'),
+  ('Missão 4'),
+  ('Missão 5'),
+  ('Missão 6'),
+  ('Missão 7'),
+  ('Missão 8'),
+  ('Missão 9'),
+  ('Missão 10');
+INSERT INTO [dbo].[tbl_politicadisciplinar] ([descricao])
 VALUES
-  ('Política 11'),
-  ('Política 12'),
-  ('Política 13'),
-  ('Política 14'),
-  ('Política 15'),
-  ('Política 16'),
-  ('Política 17'),
-  ('Política 18'),
-  ('Política 19'),
-  ('Política 20');
-
--- Inserir mais 10 valores fictícios na tabela tbl_empresa
-INSERT INTO dbo.tbl_empresa (senha, razao_social, cnpj, nome_responsavel, cpf_responsavel, logradouro, numero, complemento, bairro, cep, cidade, uf, telefone, telefone2, email, status, id_missaovisaovalores, id_politicadisciplinar)
+  ('Política 1'),
+  ('Política 2'),
+  ('Política 3'),
+  ('Política 4'),
+  ('Política 5'),
+  ('Política 6'),
+  ('Política 7'),
+  ('Política 8'),
+  ('Política 9'),
+  ('Política 10');
+INSERT INTO [dbo].[tbl_empresa] (
+  [senha], [razao_social], [cnpj], [nome_responsavel], [cpf_responsavel],
+  [logradouro], [numero], [complemento], [bairro], [cep], [cidade], [uf],
+  [telefone], [telefone2], [email], [status], [id_missaovisaovalores], [id_politicadisciplinar])
 VALUES
-  ('SenhaEmpresa11', 'Empresa 11', '12345678901111', 'Responsável 11', '11111111111', 'Rua 11', '1111', 'Sala 11A', 'Bairro 11', '11111-111', 'Cidade 11', 'SP', '(11) 1111-1111', '(11) 1111-1111', 'empresa11@example.com', 'Ativo', 1, 1),
-  ('SenhaEmpresa12', 'Empresa 12', '12345678901222', 'Responsável 12', '22222222222', 'Rua 12', '1222', 'Sala 12B', 'Bairro 12', '22222-222', 'Cidade 12', 'RJ', '(22) 2222-2222', '(22) 2222-2222', 'empresa12@example.com', 'Ativo', 2, 2),
-  ('SenhaEmpresa13', 'Empresa 13', '12345678901333', 'Responsável 13', '33333333333', 'Rua 13', '1333', 'Sala 13C', 'Bairro 13', '33333-333', 'Cidade 13', 'MG', '(33) 3333-3333', '(33) 3333-3333', 'empresa13@example.com', 'Ativo', 3, 3),
-  ('SenhaEmpresa14', 'Empresa 14', '12345678901444', 'Responsável 14', '44444444444', 'Rua 14', '1444', 'Sala 14D', 'Bairro 14', '44444-444', 'Cidade 14', 'RS', '(44) 4444-4444', '(44) 4444-4444', 'empresa14@example.com', 'Ativo', 4, 4),
-  ('SenhaEmpresa15', 'Empresa 15', '12345678901555', 'Responsável 15', '55555555555', 'Rua 15', '1555', 'Sala 15E', 'Bairro 15', '55555-555', 'Cidade 15', 'BA', '(55) 5555-5555', '(55) 5555-5555', 'empresa15@example.com', 'Ativo', 5, 5),
-  ('SenhaEmpresa16', 'Empresa 16', '12345678901666', 'Responsável 16', '66666666666', 'Rua 16', '1666', 'Sala 16F', 'Bairro 16', '66666-666', 'Cidade 16', 'CE', '(66) 6666-6666', '(66) 6666-6666', 'empresa16@example.com', 'Ativo', 6, 6),
-  ('SenhaEmpresa17', 'Empresa 17', '12345678901777', 'Responsável 17', '77777777777', 'Rua 17', '1777', 'Sala 17G', 'Bairro 17', '77777-777', 'Cidade 17', 'PE', '(77) 7777-7777', '(77) 7777-7777', 'empresa17@example.com', 'Ativo', 7, 7),
-  ('SenhaEmpresa18', 'Empresa 18', '12345678901888', 'Responsável 18', '88888888888', 'Rua 18', '1888', 'Sala 18H', 'Bairro 18', '88888-888', 'Cidade 18', 'PR', '(88) 8888-8888', '(88) 8888-8888', 'empresa18@example.com', 'Ativo', 8, 8),
-  ('SenhaEmpresa19', 'Empresa 19', '12345678901999', 'Responsável 19', '99999999999', 'Rua 19', '1999', 'Sala 19I', 'Bairro 19', '99999-999', 'Cidade 19', 'SC', '(99) 9999-9999', '(99) 9999-9999', 'empresa19@example.com', 'Ativo', 9, 9),
-  ('SenhaEmpresa20', 'Empresa 20', '12345678902020', 'Responsável 20', '20202020202', 'Rua 20', '2020', 'Sala 20J', 'Bairro 20', '20202-020', 'Cidade 20', 'GO', '(20) 2020-2020', '(20) 2020-2020', 'empresa20@example.com', 'Ativo', 10, 10);
-
--- Inserir mais 10 valores fictícios na tabela tbl_normaregra
-INSERT INTO dbo.tbl_normaregra (tipo, descricao, id_empresa)
+  ('senha1', 'Empresa 1', '1234567890', 'Responsável 1', '12345678901',
+   'Rua A', '123', NULL, 'Bairro 1', '12345-678', 'Cidade 1', 'UF1',
+   '123-456-7890', '987-654-3210', 'empresa1@email.com', 'Ativa', 1, 1),
+  ('senha2', 'Empresa 2', '2345678901', 'Responsável 2', '23456789012',
+   'Rua B', '456', 'Complemento 2', 'Bairro 2', '23456-789', 'Cidade 2', 'UF2',
+   '234-567-8901', '876-543-2109', 'empresa2@email.com', 'Inativa', 2, 2),
+  ('senha3', 'Empresa 3', '3456789012', 'Responsável 3', '34567890123',
+   'Rua C', '789', NULL, 'Bairro 3', '34567-890', 'Cidade 3', 'UF3',
+   '345-678-9012', '765-432-1098', 'empresa3@email.com', 'Ativa', 3, 3),
+  ('senha4', 'Empresa 4', '4567890123', 'Responsável 4', '45678901234',
+   'Rua D', '101', 'Complemento 4', 'Bairro 4', '45678-901', 'Cidade 4', 'UF4',
+   '456-789-0123', '654-321-0987', 'empresa4@email.com', 'Ativa', 4, 4),
+  ('senha5', 'Empresa 5', '5678901234', 'Responsável 5', '56789012345',
+   'Rua E', '202', NULL, 'Bairro 5', '56789-012', 'Cidade 5', 'UF5',
+   '567-890-1234', '543-210-9876', 'empresa5@email.com', 'Inativa', 5, 5),
+  ('senha6', 'Empresa 6', '6789012345', 'Responsável 6', '67890123456',
+   'Rua F', '303', 'Complemento 6', 'Bairro 6', '67890-123', 'Cidade 6', 'UF6',
+   '678-901-2345', '432-109-8765', 'empresa6@email.com', 'Ativa', 6, 6),
+  ('senha7', 'Empresa 7', '7890123456', 'Responsável 7', '78901234567',
+   'Rua G', '404', NULL, 'Bairro 7', '78901-234', 'Cidade 7', 'UF7',
+   '789-012-3456', '321-098-7654', 'empresa7@email.com', 'Ativa', 7, 7),
+  ('senha8', 'Empresa 8', '8901234567', 'Responsável 8', '89012345678',
+   'Rua H', '505', 'Complemento 8', 'Bairro 8', '89012-345', 'Cidade 8', 'UF8',
+   '890-123-4567', '210-987-6543', 'empresa8@email.com', 'Inativa', 8, 8),
+  ('senha9', 'Empresa 9', '9012345678', 'Responsável 9', '90123456789',
+   'Rua I', '606', NULL, 'Bairro 9', '90123-456', 'Cidade 9', 'UF9',
+   '901-234-5678', '098-765-4321', 'empresa9@email.com', 'Ativa', 9, 9),
+  ('senha10', 'Empresa 10', '0123456789', 'Responsável 10', '01234567890',
+   'Rua J', '707', 'Complemento 10', 'Bairro 10', '01234-567', 'Cidade 10', 'UF10',
+   '012-345-6789', '987-654-3210', 'empresa10@email.com', 'Ativa', 10, 10);
+INSERT INTO [dbo].[tbl_normaregra] ([tipo], [descricao], [id_empresa])
 VALUES
-  ('Regra 2', 'Regra 2 de Conduta', 2),
-  ('Regra 3', 'Regra 3 de Conduta', 3),
-  ('Regra 4', 'Regra 4 de Conduta', 4),
-  ('Regra 5', 'Regra 5 de Conduta', 5),
-  ('Regra 6', 'Regra 6 de Conduta', 6),
-  ('Regra 7', 'Regra 7 de Conduta', 7),
-  ('Regra 8', 'Regra 8 de Conduta', 8),
-  ('Regra 9', 'Regra 9 de Conduta', 9),
-  ('Regra 10', 'Regra 10 de Conduta', 10);
-
--- Inserir mais 10 valores fictícios na tabela tbl_setor
-INSERT INTO dbo.tbl_setor (nome_setor, id_empresa)
+  ('Tipo 1', 'Regra 1', 1),
+  ('Tipo 2', 'Regra 2', 2),
+  ('Tipo 3', 'Regra 3', 3),
+  ('Tipo 1', 'Regra 4', 4),
+  ('Tipo 2', 'Regra 5', 5),
+  ('Tipo 3', 'Regra 6', 6),
+  ('Tipo 1', 'Regra 7', 7),
+  ('Tipo 2', 'Regra 8', 8),
+  ('Tipo 3', 'Regra 9', 9),
+  ('Tipo 1', 'Regra 10', 10);
+INSERT INTO [dbo].[tbl_setor] ([nome_setor], [id_empresa])
 VALUES
-  ('Departamento 2', 1),
-  ('Departamento 3', 2),
-  ('Departamento 4', 3),
-  ('Departamento 5', 4),
-  ('Departamento 6', 5),
-  ('Departamento 7', 6),
-  ('Departamento 8', 7),
-  ('Departamento 9', 8),
-  ('Departamento 10', 9);
-
--- Inserir mais 10 valores fictícios na tabela tbl_colaborador
-INSERT INTO dbo.tbl_colaborador (nome, data_nascimento, cpf, tipo_contrato, salario_bruto, senha, carga_horaria, logradouro, numero, complemento, bairro, cep, cidade, uf, telefone, telefone2, email, id_setor)
+  ('Setor 1', 1),
+  ('Setor 2', 2),
+  ('Setor 3', 3),
+  ('Setor 4', 4),
+  ('Setor 5', 5),
+  ('Setor 6', 6),
+  ('Setor 7', 7),
+  ('Setor 8', 8),
+  ('Setor 9', 9),
+  ('Setor 10', 10);
+INSERT INTO [dbo].[tbl_colaborador] (
+  [nome], [data_nascimento], [cpf], [tipo_contrato], [salario_bruto], [senha],
+  [carga_horaria], [logradouro], [numero], [complemento], [bairro], [cep], [cidade], [uf],
+  [telefone], [telefone2], [email], [id_setor])
 VALUES
-  ('Carlos Rodrigues', '1985-09-20', '12345678912', 'CLT', 4000.00, 'SenhaColaborador11', 40, 'Av. 21', '21', 'Bloco B', 'Bairro C', '21212-121', 'Cidade C', 'RJ', '(21) 2121-2121', '(21) 9876-5432', 'carlos@example.com', 1),
-  ('Ana Silva', '1992-03-10', '98765432123', 'CLT', 3500.00, 'SenhaColaborador12', 40, 'Rua 22', '22', 'Sala 22A', 'Bairro D', '22222-222', 'Cidade D', 'SP', '(22) 2222-2222', '(22) 9876-5432', 'ana@example.com', 2),
-  ('Roberto Pereira', '1988-06-15', '23456789034', 'CLT', 3800.00, 'SenhaColaborador13', 40, 'Rua 23', '23', 'Sala 23B', 'Bairro E', '23232-323', 'Cidade E', 'MG', '(33) 3333-3333', '(33) 9876-5432', 'roberto@example.com', 3),
-  ('Carla Santos', '1990-01-05', '34567890145', 'CLT', 4200.00, 'SenhaColaborador14', 40, 'Av. 24', '24', 'Bloco C', 'Bairro F', '24242-424', 'Cidade F', 'RS', '(44) 4444-4444', '(44) 9876-5432', 'carla@example.com', 4),
-  ('Paulo Oliveira', '1983-12-30', '45678901256', 'CLT', 3700.00, 'SenhaColaborador15', 40, 'Rua 25', '25', 'Sala 25D', 'Bairro G', '25252-525', 'Cidade G', 'BA', '(55) 5555-5555', '(55) 9876-5432', 'paulo@example.com', 5),
-  ('Fernanda Lima', '1995-04-18', '56789012367', 'CLT', 3900.00, 'SenhaColaborador16', 40, 'Av. 26', '26', 'Sala 26E', 'Bairro H', '26262-626', 'Cidade H', 'PE', '(66) 6666-6666', '(66) 9876-5432', 'fernanda@example.com', 6),
-  ('Ricardo Sousa', '1987-07-22', '67890123478', 'CLT', 4100.00, 'SenhaColaborador17', 40, 'Rua 27', '27', 'Sala 27F', 'Bairro I', '27272-727', 'Cidade I', 'PR', '(77) 7777-7777', '(77) 9876-5432', 'ricardo@example.com', 7),
-  ('Sandra Almeida', '1998-10-12', '78901234589', 'CLT', 3600.00, 'SenhaColaborador18', 40, 'Av. 28', '28', 'Sala 28G', 'Bairro J', '28282-828', 'Cidade J', 'SC', '(88) 8888-8888', '(88) 9876-5432', 'sandra@example.com', 8),
-  ('Márcio Pereira', '1980-02-25', '89012345690', 'CLT', 4300.00, 'SenhaColaborador19', 40, 'Rua 29', '29', 'Sala 29H', 'Bairro K', '29292-929', 'Cidade K', 'GO', '(99) 9999-9999', '(99) 9876-5432', 'marcio@example.com', 9);
-
--- Inserir mais 10 valores fictícios na tabela tbl_folhadepagamento
-INSERT INTO dbo.tbl_folhadepagamento (valor_folhafinal, valor_desc_total, horas_trab, salario_liq, periodo_inicio, periodo_fim, id_empresa)
+  ('Colaborador 1', '1990-01-01', '12345678901', 'Contrato 1', 3500.00, 'senha1',
+   40, 'Rua A', '101', NULL, 'Bairro 1', '12345-678', 'Cidade 1', 'UF1',
+   '123-456-7890', '987-654-3210', 'colaborador1@email.com', 1),
+  ('Colaborador 2', '1985-02-15', '23456789012', 'Contrato 2', 4200.00, 'senha2',
+   35, 'Rua B', '202', 'Complemento 2', 'Bairro 2', '23456-789', 'Cidade 2', 'UF2',
+   '234-567-8901', '876-543-2109', 'colaborador2@email.com', 2),
+  ('Colaborador 3', '1993-03-25', '34567890123', 'Contrato 3', 3800.00, 'senha3',
+   38, 'Rua C', '303', NULL, 'Bairro 3', '34567-890', 'Cidade 3', 'UF3',
+   '345-678-9012', '765-432-1098', 'colaborador3@email.com', 3),
+  ('Colaborador 4', '1988-04-10', '45678901234', 'Contrato 1', 3200.00, 'senha4',
+   40, 'Rua D', '404', 'Complemento 4', 'Bairro 4', '45678-901', 'Cidade 4', 'UF4',
+   '456-789-0123', '654-321-0987', 'colaborador4@email.com', 4),
+  ('Colaborador 5', '1995-05-20', '56789012345', 'Contrato 2', 4000.00, 'senha5',
+   35, 'Rua E', '505', NULL, 'Bairro 5', '56789-012', 'Cidade 5', 'UF5',
+   '567-890-1234', '543-210-9876', 'colaborador5@email.com', 5),
+  ('Colaborador 6', '1992-06-30', '67890123456', 'Contrato 3', 3600.00, 'senha6',
+   38, 'Rua F', '606', 'Complemento 6', 'Bairro 6', '67890-123', 'Cidade 6', 'UF6',
+   '678-901-2345', '432-109-8765', 'colaborador6@email.com', 6),
+  ('Colaborador 7', '1991-07-05', '78901234567', 'Contrato 1', 3300.00, 'senha7',
+   40, 'Rua G', '707', NULL, 'Bairro 7', '78901-234', 'Cidade 7', 'UF7',
+   '789-012-3456', '321-098-7654', 'colaborador7@email.com', 7),
+  ('Colaborador 8', '1987-08-15', '89012345678', 'Contrato 2', 4100.00, 'senha8',
+   35, 'Rua H', '808', 'Complemento 8', 'Bairro 8', '89012-345', 'Cidade 8', 'UF8',
+   '890-123-4567', '210-987-6543', 'colaborador8@email.com', 8),
+  ('Colaborador 9', '1994-09-22', '90123456789', 'Contrato 3', 3700.00, 'senha9',
+   38, 'Rua I', '909', NULL, 'Bairro 9', '90123-456', 'Cidade 9', 'UF9',
+   '901-234-5678', '098-765-4321', 'colaborador9@email.com', 9),
+  ('Colaborador 10', '1986-10-03', '01234567890', 'Contrato 1', 3400.00, 'senha10',
+   40, 'Rua J', '1010', 'Complemento 10', 'Bairro 10', '01234-567', 'Cidade 10', 'UF10',
+   '012-345-6789', '987-654-3210', 'colaborador10@email.com', 10);
+INSERT INTO [dbo].[tbl_folhadepagamento] (
+  [valor_folhafinal], [valor_desc_total], [horas_trab], [salario_liq],
+  [periodo_inicio], [periodo_fim], [id_empresa])
 VALUES
-  (3800.00, 450.00, 152, 3350.00, '2023-09-01', '2023-09-30', 1),
-  (4200.00, 520.00, 168, 3680.00, '2023-09-01', '2023-09-30', 2),
-  (3900.00, 480.00, 156, 3420.00, '2023-09-01', '2023-09-30', 3),
-  (3700.00, 460.00, 148, 3240.00, '2023-09-01', '2023-09-30', 4),
-  (4100.00, 510.00, 164, 3590.00, '2023-09-01', '2023-09-30', 5),
-  (3600.00, 440.00, 140, 3080.00, '2023-09-01', '2023-09-30', 6),
-  (4300.00, 530.00, 172, 3770.00, '2023-09-01', '2023-09-30', 7),
-  (3950.00, 490.00, 158, 3460.00, '2023-09-01', '2023-09-30', 8),
-  (4150.00, 520.00, 166, 3630.00, '2023-09-01', '2023-09-30', 8);
-
--- Inserir mais 10 valores fictícios na tabela tbl_folhaindividual
-INSERT INTO dbo.tbl_folhaindividual (periodo_inicio, periodo_fim, valor_folhafinal, valor_desc_total, horas_trab, salario_liq, id_folhadepagamento, id_colaborador)
+  (3500.00, 300.00, 160.00, 3200.00, '2023-08-01', '2023-08-31', 1),
+  (4200.00, 350.00, 140.00, 3850.00, '2023-08-01', '2023-08-31', 2),
+  (3800.00, 320.00, 152.00, 3480.00, '2023-08-01', '2023-08-31', 3),
+  (3200.00, 280.00, 160.00, 2920.00, '2023-08-01', '2023-08-31', 4),
+  (4000.00, 340.00, 136.00, 3660.00, '2023-08-01', '2023-08-31', 5),
+  (3600.00, 310.00, 156.00, 3290.00, '2023-08-01', '2023-08-31', 6),
+  (3300.00, 290.00, 160.00, 3010.00, '2023-08-01', '2023-08-31', 7),
+  (4100.00, 350.00, 140.00, 3760.00, '2023-08-01', '2023-08-31', 8),
+  (3700.00, 320.00, 152.00, 3380.00, '2023-08-01', '2023-08-31', 9),
+  (3400.00, 280.00, 160.00, 3120.00, '2023-08-01', '2023-08-31', 10);
+INSERT INTO [dbo].[tbl_folhaindividual] (
+  [periodo_inicio], [periodo_fim], [valor_folhafinal], [valor_desc_total],
+  [horas_trab], [salario_liq], [id_folhadepagamento], [id_colaborador])
 VALUES
-  ('2023-09-01', '2023-09-30', 3800.00, 450.00, 152, 3350.00, 1, 1),
-  ('2023-09-01', '2023-09-30', 4200.00, 520.00, 168, 3680.00, 2, 2),
-  ('2023-09-01', '2023-09-30', 3900.00, 480.00, 156, 3420.00, 3, 3),
-  ('2023-09-01', '2023-09-30', 3700.00, 460.00, 148, 3240.00, 4, 4),
-  ('2023-09-01', '2023-09-30', 4100.00, 510.00, 164, 3590.00, 5, 5),
-  ('2023-09-01', '2023-09-30', 3600.00, 440.00, 140, 3080.00, 6, 6),
-  ('2023-09-01', '2023-09-30', 4300.00, 530.00, 172, 3770.00, 7, 7),
-  ('2023-09-01', '2023-09-30', 3950.00, 490.00, 158, 3460.00, 8, 8),
-  ('2023-09-01', '2023-09-30', 4150.00, 520.00, 166, 3630.00, 9, 9);
-
--- Inserir mais 10 valores fictícios na tabela tbl_holerite
-INSERT INTO dbo.tbl_holerite (horas_trab, valor_desc_total, salario_liq, id_folhadepagamento, id_colaborador)
+  ('2023-08-01', '2023-08-31', 3200.00, 300.00, 160.00, 3200.00, 1, 1),
+  ('2023-08-01', '2023-08-31', 3850.00, 350.00, 140.00, 3850.00, 2, 2),
+  ('2023-08-01', '2023-08-31', 3480.00, 320.00, 152.00, 3480.00, 3, 3),
+  ('2023-08-01', '2023-08-31', 2920.00, 280.00, 160.00, 2920.00, 4, 4),
+  ('2023-08-01', '2023-08-31', 3660.00, 340.00, 136.00, 3660.00, 5, 5),
+  ('2023-08-01', '2023-08-31', 3290.00, 310.00, 156.00, 3290.00, 6, 6),
+  ('2023-08-01', '2023-08-31', 3010.00, 290.00, 160.00, 3010.00, 7, 7),
+  ('2023-08-01', '2023-08-31', 3760.00, 350.00, 140.00, 3760.00, 8, 8),
+  ('2023-08-01', '2023-08-31', 3380.00, 320.00, 152.00, 3380.00, 9, 9),
+  ('2023-08-01', '2023-08-31', 3120.00, 280.00, 160.00, 3120.00, 10, 10);
+INSERT INTO [dbo].[tbl_holerite] (
+  [horas_trab], [valor_desc_total], [salario_liq], [id_folhadepagamento], [id_colaborador])
 VALUES
-  (160, 50.00, 3500.00, 1, 1),
-  (152, 45.00, 3350.00, 2, 2),
-  (168, 52.00, 3680.00, 3, 3),
-  (156, 48.00, 3420.00, 4, 4),
-  (148, 46.00, 3240.00, 5, 5),
-  (164, 51.00, 3590.00, 6, 6),
-  (140, 44.00, 3080.00, 7, 7),
-  (172, 53.00, 3770.00, 8, 8),
-  (158, 49.00, 3460.00, 9, 9),
-  (166, 52.00, 3630.00, 9, 9);
-
--- Inserir mais 10 valores fictícios na tabela tbl_pontoeletronico
-INSERT INTO dbo.tbl_pontoeletronico (data, entrada, saida_almoco, retorno_almoco, saida, tipoJustificativa, id_colaborador)
+  (160.00, 30.00, 3200.00, 1, 1),
+  (140.00, 35.00, 3850.00, 2, 2),
+  (152.00, 32.00, 3480.00, 3, 3),
+  (160.00, 28.00, 2920.00, 4, 4),
+  (136.00, 34.00, 3660.00, 5, 5),
+  (156.00, 31.00, 3290.00, 6, 6),
+  (160.00, 29.00, 3010.00, 7, 7),
+  (140.00, 35.00, 3760.00, 8, 8),
+  (152.00, 32.00, 3380.00, 9, 9),
+  (160.00, 28.00, 3120.00, 10, 10);
+INSERT INTO [dbo].[tbl_pontoeletronico] (
+  [data], [entrada], [saida_almoco], [retorno_almoco], [saida], [tipoJustificativa],
+  [descricao], [documento], [id_colaborador])
 VALUES
-  ('2023-09-01', '08:00:00', '12:00:00', '13:00:00', '17:00:00', 'N/A', 1),
-  ('2023-09-01', '08:15:00', '12:15:00', '13:15:00', '17:15:00', 'N/A', 1),
-  ('2023-09-01', '08:30:00', '12:30:00', '13:30:00', '17:30:00', 'N/A', 2),
-  ('2023-09-01', '08:45:00', '12:45:00', '13:45:00', '17:45:00', 'N/A', 3),
-  ('2023-09-01', '09:00:00', '13:00:00', '14:00:00', '18:00:00', 'N/A', 4),
-  ('2023-09-01', '09:15:00', '13:15:00', '14:15:00', '18:15:00', 'N/A', 5),
-  ('2023-09-01', '09:30:00', '13:30:00', '14:30:00', '18:30:00', 'N/A', 6),
-  ('2023-09-01', '09:45:00', '13:45:00', '14:45:00', '18:45:00', 'N/A', 7),
-  ('2023-09-01', '10:00:00', '14:00:00', '15:00:00', '19:00:00', 'N/A', 8),
-  ('2023-09-01', '10:15:00', '14:15:00', '15:15:00', '19:15:00', 'N/A', 9);
+  ('2023-08-01 00:00:00', '08:00:00', '12:00:00', '13:00:00', '17:00:00', 'Entrada', 'Ponto do dia 1', NULL, 1),
+  ('2023-08-02 00:00:00', '08:15:00', '12:30:00', '13:30:00', '17:15:00', 'Entrada', 'Ponto do dia 2', NULL, 2),
+  ('2023-08-03 00:00:00', '08:10:00', '12:20:00', '13:20:00', '17:10:00', 'Entrada', 'Ponto do dia 3', NULL, 3),
+  ('2023-08-04 00:00:00', '08:05:00', '12:10:00', '13:10:00', '17:05:00', 'Entrada', 'Ponto do dia 4', NULL, 4),
+  ('2023-08-05 00:00:00', '08:30:00', '12:45:00', '13:45:00', '17:30:00', 'Entrada', 'Ponto do dia 5', NULL, 5),
+  ('2023-08-06 00:00:00', '08:20:00', '12:40:00', '13:40:00', '17:20:00', 'Entrada', 'Ponto do dia 6', NULL, 6),
+  ('2023-08-07 00:00:00', '08:25:00', '12:35:00', '13:35:00', '17:25:00', 'Entrada', 'Ponto do dia 7', NULL, 7),
+  ('2023-08-08 00:00:00', '08:15:00', '12:30:00', '13:30:00', '17:15:00', 'Entrada', 'Ponto do dia 8', NULL, 8),
+  ('2023-08-09 00:00:00', '08:10:00', '12:20:00', '13:20:00', '17:10:00', 'Entrada', 'Ponto do dia 9', NULL, 9),
+  ('2023-08-10 00:00:00', '08:00:00', '12:00:00', '13:00:00', '17:00:00', 'Entrada', 'Ponto do dia 10', NULL, 10);
