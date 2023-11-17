78da111cfa78098b9c3a3a5e7f6b2bf4a11d7683
Mbaroone <mauricio.fsantos@outlook.com>
2023-11-17 15:06:25 -0300

    tratamento da grid

--- Desktop/deltarh/delta_modelo/mdlPontoEletronico.cs
+++ Desktop/deltarh/delta_modelo/mdlPontoEletronico.cs
@@ -1,21 +1,41 @@
 ﻿using System;
+using System.ComponentModel;
 
 namespace delta_modelo
 {
     public class mdlPontoEletronico
     {
+        [Browsable(false)]
         public int id_pontoeletronico { get; set; }
+
+        [Browsable(true)]
         public DateTime data { get; set; }
+
+        [Browsable(true)]
         public TimeSpan? entrada { get; set; }
+
+        [Browsable(true)]
         public TimeSpan? saida_almoco { get; set; }
 
+        [Browsable(true)]
         public TimeSpan? retorno_almoco { get; set; }
 
+        [Browsable(true)]
         public TimeSpan? saida { get; set; }
+
+        [Browsable(false)]
         public string tipo_justificativa { get; set; }
+
+        [Browsable(false)]
         public string descricao { get; set; }
+
+        [Browsable(false)]
         public Byte[] documento { get; set; }
+
+        [Browsable(false)]
         public int id_colaborador { get; set; }
+
+        [Browsable(false)]
         public bool abono { get; set; }
     }
 }
--- Desktop/deltarh/deltarh/FrmFolhaIndividual.Designer.cs
+++ Desktop/deltarh/deltarh/FrmFolhaIndividual.Designer.cs
@@ -993,7 +993,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1064, 537);
+            this.ClientSize = new System.Drawing.Size(1064, 495);
             this.Controls.Add(this.chBoxAbonar);
             this.Controls.Add(this.btnSalvar);
             this.Controls.Add(this.picBoxComprovante);
