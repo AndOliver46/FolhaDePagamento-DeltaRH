9d1f413b140f1cc96bb04bdea412ec3e80951a4c
Leonardo-Azevedo <leonardo.morcego@hotmail.com>
2023-11-18 17:44:10 -0300

    Mensagem de erro: Sessao finalizada

--- Web/DeltaRHWeb/wwwroot/js/script-login.js
+++ Web/DeltaRHWeb/wwwroot/js/script-login.js
@@ -1,8 +1,10 @@
 const urlParams = new URLSearchParams(window.location.search);
 const myParam = urlParams.get('error');
 const myParam2 = urlParams.get('statuserror');
+const myParam3 = urlParams.get('sessaoInvalida');
 const paragrafoErro = document.getElementById("paragrafo-erro");
 const paragrafoErroSt = document.getElementById("paragrafo-erro-st");
+const paragrafoErroSi = document.getElementById("paragrafo-erro-si");
 console.log(myParam)
 
 if (myParam == "true") {
@@ -11,4 +13,8 @@ if (myParam == "true") {
 
 if (myParam2 == "true") {
     paragrafoErroSt.style.display = "block";
+}
+
+if (myParam3 == "true") {
+    paragrafoErroSi.style.display = "block"
 }
