3658cd23ab14b365343f8642905e4b9081c69030
Wagner Scchunck <115500423+wagnerschunck@users.noreply.github.com>
2023-11-27 22:28:53 -0300

    FINAL

--- script-dados-ficticios.sql
+++ script-dados-ficticios.sql
@@ -1,111 +1,50 @@
--- Inserção de 10 registros na tabela tbl_missaovisaovalores
-INSERT INTO [dbo].[tbl_missaovisaovalores] ([descricao])
-VALUES
-('Missão 1'),
-('Missão 2'),
-('Missão 3'),
-('Missão 4'),
-('Missão 5'),
-('Missão 6'),
-('Missão 7'),
-('Missão 8'),
-('Missão 9'),
-('Missão 10');
 
--- Inserção de 10 registros na tabela tbl_politicadisciplinar
-INSERT INTO [dbo].[tbl_politicadisciplinar] ([descricao])
-VALUES
-('Política 1'),
-('Política 2'),
-('Política 3'),
-('Política 4'),
-('Política 5'),
-('Política 6'),
-('Política 7'),
-('Política 8'),
-('Política 9'),
-('Política 10');
 
 -- Inserção de 10 registros na tabela tbl_empresa
 INSERT INTO [dbo].[tbl_empresa] ([senha], [razao_social], [cnpj], [nome_responsavel], [cpf_responsavel], [logradouro], [numero], [complemento], [bairro], [cep], [cidade], [uf], [telefone], [telefone2], [email], [status], [vt], [vr], [ass_medica], [odonto], [gympass], [id_missaovisaovalores], [id_politicadisciplinar])
 VALUES
-('senha1', 'Empresa 1', '1234567890', 'Responsável 1', '12345678901', 'Endereço 1', '123', 'Comp. 1', 'Bairro 1', '12345-678', 'Cidade 1', 'UF1', '123-4567-8901', '123-4567-8902', 'email1@empresa.com', 'Ativo', 1.00, 2.00, 3.00, 4.00, 5.00, 1, 1),
-('senha2', 'Empresa 2', '2345678901', 'Responsável 2', '23456789012', 'Endereço 2', '234', 'Comp. 2', 'Bairro 2', '23456-789', 'Cidade 2', 'UF2', '234-5678-9012', '234-5678-9013', 'email2@empresa.com', 'Inativo', 1.00, 2.00, 3.00, 4.00, 5.00, 2, 2),
-('senha3', 'Empresa 3', '3456789012', 'Responsável 3', '34567890123', 'Endereço 3', '345', 'Comp. 3', 'Bairro 3', '34567-890', 'Cidade 3', 'UF3', '345-6789-0123', '345-6789-0124', 'email3@empresa.com', 'Ativo', 2.00, 3.00, 4.00, 5.00, 6.00, 3, 3),
-('senha4', 'Empresa 4', '4567890123', 'Responsável 4', '45678901234', 'Endereço 4', '456', 'Comp. 4', 'Bairro 4', '45678-901', 'Cidade 4', 'UF4', '456-7890-1234', '456-7890-1235', 'email4@empresa.com', 'Inativo', 2.00, 3.00, 4.00, 5.00, 6.00, 4, 4),
-('senha5', 'Empresa 5', '5678901234', 'Responsável 5', '56789012345', 'Endereço 5', '567', 'Comp. 5', 'Bairro 5', '56789-012', 'Cidade 5', 'UF5', '567-8901-2345', '567-8901-2346', 'email5@empresa.com', 'Ativo', 3.00, 4.00, 5.00, 6.00, 7.00, 5, 5),
-('senha6', 'Empresa 6', '6789012345', 'Responsável 6', '67890123456', 'Endereço 6', '678', 'Comp. 6', 'Bairro 6', '67890-123', 'Cidade 6', 'UF6', '678-9012-3456', '678-9012-3457', 'email6@empresa.com', 'Inativo', 3.00, 4.00, 5.00, 6.00, 7.00, 6, 6),
-('senha7', 'Empresa 7', '7890123456', 'Responsável 7', '78901234567', 'Endereço 7', '789', 'Comp. 7', 'Bairro 7', '78901-234', 'Cidade 7', 'UF7', '789-0123-4567', '789-0123-4568', 'email7@empresa.com', 'Ativo', 4.00, 5.00, 6.00, 7.00, 8.00, 7, 7),
-('senha8', 'Empresa 8', '8901234567', 'Responsável 8', '89012345678', 'Endereço 8', '890', 'Comp. 8', 'Bairro 8', '89012-345', 'Cidade 8', 'UF8', '890-1234-5678', '890-1234-5679', 'email8@empresa.com', 'Inativo', 4.00, 5.00, 6.00, 7.00, 8.00, 8, 8),
-('senha9', 'Empresa 9', '9012345678', 'Responsável 9', '90123456789', 'Endereço 9', '901', 'Comp. 9', 'Bairro 9', '90123-456', 'Cidade 9', 'UF9', '901-2345-6789', '901-2345-6790', 'email9@empresa.com', 'Ativo', 5.00, 6.00, 7.00, 8.00, 9.00, 9, 9),
-('senha10', 'Empresa 10', '0123456789', 'Responsável 10', '01234567890', 'Endereço 10', '0123', 'Comp. 10', 'Bairro 10', '01234-567', 'Cidade 10', 'UF10', '012-3456-7890', '012-3456-7891', 'email10@empresa.com', 'Inativo', 5.00, 6.00, 7.00, 8.00, 9.00, 10, 10);
+('leroymerlin', 'Leroy Merlin', '01438784000105', 'El Leroy Che', '77788899901', 'Avenida', '456', '´Prédio', 'Alaconda', '05815-000', 'São Paulo', 'SP', '162-4967-0901', '249-6577-0902', 'leroy@comercial.com', 'Ativo', 1.00, 1.00, 3.00, 3.00, 2.00, 1, 1),
+('ibm', 'ibm', '33372251010038', 'Austin Mayer', '55596314403', 'Rua Jose', '1', 'Conjunto', 'Ipiranga', '56900-455', 'São Paulo', 'SP', '231-5118-9552', '330-5668-9993', 'ibm@comercial.com', 'ativo', 1.00, 1.00, 2.00, 4.00, 2.00, 2, 2),
+('cocacola', 'Coca Cola', '45997418000153', 'The Coco Colo', '54678912300', 'Rua Doce', '69', 'Fábrica', 'Liberdade', '34333-890', 'Niterói', 'RJ', '113-0895-7594', '363-2226-1456', 'cocacola@comercial.com', 'Ativo', 2.00, 3.00, 4.00, 2.00, 3.00, 3, 3),
+('globo', 'GLOBO', '27865757000102', 'Maranhão do Sul', '89855500647', 'Rua das Celebridades', '6', 'Estúdio', 'Celebridades anonimas', '55698-901', 'Rondopólis', 'SP', '996-4230-1994', '776-7690-1035', 'globo@record.com', 'ativo', 2.00, 3.00, 4.00, 5.00, 6.00, 4, 4);
 
--- Inserção de 10 registros na tabela tbl_normaregra
-INSERT INTO [dbo].[tbl_normaregra] ([tipo], [descricao], [id_empresa])
-VALUES
-('Tipo 1', 'Regra 1', 1),
-('Tipo 2', 'Regra 2', 2),
-('Tipo 3', 'Regra 3', 3),
-('Tipo 1', 'Regra 4', 4),
-('Tipo 2', 'Regra 5', 5),
-('Tipo 3', 'Regra 6', 6),
-('Tipo 1', 'Regra 7', 7),
-('Tipo 2', 'Regra 8', 8),
-('Tipo 3', 'Regra 9', 9),
-('Tipo 1', 'Regra 10', 10);
-
--- Inserção de 10 registros na tabela tbl_setor
-INSERT INTO [dbo].[tbl_setor] ([nome_setor], [id_empresa])
-VALUES
-('Setor 1', 1),
-('Setor 2', 2),
-('Setor 3', 3),
-('Setor 4', 4),
-('Setor 5', 5),
-('Setor 6', 6),
-('Setor 7', 7),
-('Setor 8', 8),
-('Setor 9', 9),
-('Setor 10', 10);
 
 -- Inserção de 10 registros na tabela tbl_colaborador
 INSERT INTO [dbo].[tbl_colaborador] ([nome], [data_nascimento], [cpf], [tipo_contrato], [salario_bruto], [senha], [carga_horaria], [logradouro], [numero], [complemento], [bairro], [cep], [cidade], [uf], [telefone], [telefone2], [email], [id_setor], [cargo], [status], [id_empresa], [horas_banco], [data_admissao])
 VALUES
-('Colaborador 1', '1990-01-01', '12345678901', 'Contrato 1', 3000.00, 'senha1', 220, 'Endereço 1', '123', 'Comp. 1', 'Bairro 1', '12345-678', 'Cidade 1', 'UF1', '123-4567-8901', '123-4567-8902', 'email1@colaborador.com', 1, 'Cargo 1', 'Ativo', 1, '08.00', '2022-01-29'),
-('Colaborador 2', '1991-02-02', '23456789012', 'Contrato 2', 3500.00, 'senha2', 220, 'Endereço 2', '234', 'Comp. 2', 'Bairro 2', '23456-789', 'Cidade 2', 'UF2', '234-5678-9012', '234-5678-9013', 'email2@colaborador.com', 1, 'Cargo 2', 'Ativo', 1, '09.00', '2023-05-27'),
-('Colaborador 3', '1992-03-03', '34567890123', 'Contrato 3', 4000.00, 'senha3', 220, 'Endereço 3', '345', 'Comp. 3', 'Bairro 3', '34567-890', 'Cidade 3', 'UF3', '345-6789-0123', '345-6789-0124', 'email3@colaborador.com', 1, 'Cargo 3', 'Inativo', 1, '10.00', '2023-07-15'),
-('Colaborador 4', '1993-04-04', '45678901234', 'Contrato 1', 3200.00, 'senha4', 220, 'Endereço 4', '456', 'Comp. 4', 'Bairro 4', '45678-901', 'Cidade 4', 'UF4', '456-7890-1234', '456-7890-1235', 'email4@colaborador.com', 1, 'Cargo 1', 'Inativo', 1, '07.00', '2020-06-14'),
-('Colaborador 5', '1994-05-05', '56789012345', 'Contrato 2', 3700.00, 'senha5', 220, 'Endereço 5', '567', 'Comp. 5', 'Bairro 5', '56789-012', 'Cidade 5', 'UF5', '567-8901-2345', '567-8901-2346', 'email5@colaborador.com', 1, 'Cargo 2', 'Ativo', 1, '08.30', '2019-05-28'),
-('Colaborador 6', '1995-06-06', '67890123456', 'Contrato 3', 4100.00, 'senha6', 220, 'Endereço 6', '678', 'Comp. 6', 'Bairro 6', '67890-123', 'Cidade 6', 'UF6', '678-9012-3456', '678-9012-3457', 'email6@colaborador.com', 1, 'Cargo 3', 'Ativo', 1, '08.00', '2020-06-23'),
-('Colaborador 7', '1996-07-07', '78901234567', 'Contrato 1', 3400.00, 'senha7', 220, 'Endereço 7', '789', 'Comp. 7', 'Bairro 7', '78901-234', 'Cidade 7', 'UF7', '789-0123-4567', '789-0123-4568', 'email7@colaborador.com', 1, 'Cargo 1', 'Inativo', 1, '09.30', '2015-07-19'),
-('Colaborador 8', '1997-08-08', '89012345678', 'Contrato 2', 3800.00, 'senha8', 220, 'Endereço 8', '890', 'Comp. 8', 'Bairro 8', '89012-345', 'Cidade 8', 'UF8', '890-1234-5678', '890-1234-5679', 'email8@colaborador.com', 8, 'Cargo 2', 'Ativo', 8, '10.00', '2023-09-01'),
-('Colaborador 9', '1998-09-09', '90123456789', 'Contrato 3', 4200.00, 'senha9', 220, 'Endereço 9', '901', 'Comp. 9', 'Bairro 9', '90123-456', 'Cidade 9', 'UF9', '901-2345-6789', '901-2345-6790', 'email9@colaborador.com', 9, 'Cargo 3', 'Inativo', 9, '07.30', '2022-11-06'),
-('Colaborador 10', '1999-10-10', '01234567890', 'Contrato 1', 3600.00, 'senha10', 220, 'Endereço 10', '0123', 'Comp. 10', 'Bairro 10', '01234-567', 'Cidade 10', 'UF10', '012-3456-7890', '012-3456-7891', 'email10@colaborador.com', 10, 'Cargo 1', 'Ativo', 10, '09.00', '2022-11-05');
+('Wagner Cristiano Santos Schunck', '1999-09-07', '47963465895', 'Fixo', 195000.00, 'wagner', 220, 'Rua Narciso Milanez', '108', 'Casa', 'Jardim Emilia', '06900-658', 'Embu-Guaçu', 'SP', '119-9722-3380', '155-0708-1102', 'assessor@adm.com', 1, 'Assessor', 'Ativo', 1, '08.00', '2022-02-22'),
+('Alessandro Delfim Louro Riedel', '1981-06-22', '24456766012', 'Fixo', 7500.00, 'alessandro', 220, 'Avenida Trinta e Três', '4', 'Casa', 'Centro', '71936-999', 'Embu das Artes', 'SP', '554-5676-9165', '115-1130-9223', 'financas@pref.com', 2, 'Secretario de Finanças', 'Ativo', 1, '08.00', '2022-07-17'),
+('Ana Paula Inacio Feitoza', '1988-12-20', '69110595481', 'Fixo', 5500.00, 'anapaula', 220, 'Estrada das Laranjeiras', '697', 'H', 'Val Flor', '82613-009', 'São Paulo', 'SP', '658-7099-0553', '997-6700-0104', 'til@pref.com', 1, 'Diretora de TI', 'ativo', 1, '08.00', '2022-08-01'),
+('Moises da Madalena', '1976-05-04', '96423700119', 'Fixo', 7500.00, 'moises', 220, 'Rua Colina Alice', '36', 'Apartamento F2', 'BeijaFLor', '74400-901', 'Embu-Guaçu', 'SP', '963-8813-1249', '714-7136-1266', 'governo@pref.com', 3, 'Secretario de Governo', 'Ativo', 1, '08.00', '2021-01-02'),
+('Marcia Ferraz Gervásio', '1988-02-05', '42668446901', 'Fixo', 4300.00, 'marcia', 220, 'Rua Jovial', '35', 'Casa', 'Vazio', '56609-011', 'São Paulo', 'SP', '887-8944-2165', '567-0530-2376', 'gabinete@pref.com', 4, 'Chefe de Gabinete', 'Ativo', 1, '08.30', '2021-05-28');
+('Josue de Alves', '1993-02-06', '32965400121', 'Fixo', 180000.00, 'josue', 220, 'Rua Adonis de Plama', '632', 'Prédio', 'Plenario do Sul', '60450-123', 'São Paulo', 'SP', '110-3090-4400', '611-6947-1157', 'adm@globo.com', 21, 'Diretor Geral', 'Ativo', 5, '10.00', '2004-11-23'),
+('Grazi Massafera', '1997-04-07', '11487569433', 'Fixo', 50400.00, 'grazi', 220, 'Estrada do Local Azul', '1000', 'Sobrado', 'Vazio', '64219-234', 'São Paulo', 'SP', '119-0123-077', '639-4944-9368', 'comercial@globo.com', 22, 'Diretora de TI', 'ativo', 5, '12.00', '2011-06-19'),
+('Boninho da Silva', '1998-02-08', '73325409805', 'Fixo', 80800.00, 'boninho', 220, 'Rua Esther de Freitas', '133', 'Apartamento Z1', 'Pedrovez', '89119-345', 'São Paulo', 'SP', '910-1694-1118', '030-1994-4419', 'ti@globo.com', 23, 'Analista Senior', 'Ativo', 5, '08.00', '2009-02-01'),
+('Luana Paes Leme', '1973-11-23', '99910498466', 'Fixo', 35000.00, 'luana', 220, 'Estrada dos Cachos', '91', 'Mansão', 'Zetto de Vele', '99664-401', 'São Paulo', 'SP', '911-6499-0110', '991-9107-6790', 'rh@globo.com', 24, 'Encarregado', 'ativo', 5, '08.30', '2001-11-06'),
+('Fernanda Montegro', '1924-10-21', '65775902406', 'Fixo', 103600.00, 'fernanda', 220, 'Rua dos Quase lá', '1000', 'Palácio', 'Avenida Dourada', '11852-567', 'São Paulo', 'SP', '912-6046-6743', '662-4458-7891', 'finanças@globo.com', 25, 'Gerente Financeiro', 'Ativo', 5, '08.00', '1995-11-05');
 
 -- Inserção de 10 registros na tabela tbl_pontoeletronico
-INSERT INTO [dbo].[tbl_pontoeletronico] 
+USE BD_DELTA
+SET DATEFORMAT ymd;  INSERT INTO [dbo].[tbl_pontoeletronico] 
 ([data], [entrada], [saida_almoco], [retorno_almoco], [saida], [tipo_justificativa], [descricao], [documento], [id_colaborador], [abono])
 VALUES
-('2023-09-01', '08:00:00', '12:00:00', '13:00:00', '17:00:00', NULL, NULL, NULL, 1, 0),
-('2023-09-04', '08:00:00', '12:00:00', '13:00:00', '17:00:00', NULL, NULL, NULL, 1, 0),
-('2023-09-05', '08:00:00', '12:00:00', '13:00:00', '17:00:00', NULL, NULL, NULL, 1, 0),
-('2023-09-06', NULL, NULL, NULL, NULL, 'FALTA', 'Faltei mesmo', NULL, 1, 0),
-('2023-09-07', '08:00:00', '12:00:00', '13:00:00', '17:00:00', NULL, NULL, NULL, 1, 0),
-('2023-09-08', '08:00:00', '12:00:00', '13:00:00', '17:00:00', NULL, NULL, NULL, 1, 0),
-('2023-09-11', '08:00:00', '12:00:00', '13:00:00', '17:00:00', NULL, NULL, NULL, 1, 0),
-('2023-09-12', '08:00:00', '12:00:00', '13:00:00', '17:00:00', NULL, NULL, NULL, 1, 0),
-('2023-09-13', '08:00:00', '12:00:00', '13:00:00', '17:00:00', NULL, NULL, NULL, 1, 0),
-('2023-09-14', '08:00:00', '12:00:00', '13:00:00', '17:00:00', NULL, NULL, NULL, 1, 0),
-('2023-09-15', '08:00:00', '12:00:00', '13:00:00', '17:00:00', NULL, NULL, NULL, 1, 0),
-('2023-09-18', '08:00:00', '12:00:00', '13:00:00', '17:00:00', NULL, NULL, NULL, 1, 0),
-('2023-09-19', '08:00:00', '12:00:00', '13:00:00', '17:00:00', NULL, NULL, NULL, 1, 0),
-('2023-09-20', '08:00:00', '12:00:00', '13:00:00', '17:00:00', NULL, NULL, NULL, 1, 0),
-('2023-09-21', '08:00:00', '12:00:00', '13:00:00', '17:00:00', NULL, NULL, NULL, 1, 0),
-('2023-09-22', '08:00:00', '12:00:00', '13:00:00', '17:00:00', NULL, NULL, NULL, 1, 0),
-('2023-09-25', '08:00:00', '12:00:00', '13:00:00', '17:00:00', NULL, NULL, NULL, 1, 0),
-('2023-09-26', '08:00:00', '12:00:00', '13:00:00', '17:00:00', NULL, NULL, NULL, 1, 0),
-('2023-09-27', '08:00:00', '12:00:00', '13:00:00', '17:00:00', NULL, NULL, NULL, 1, 0),
-('2023-09-28', '08:00:00', '12:00:00', '13:00:00', '17:00:00', NULL, NULL, NULL, 1, 0),
-('2023-09-29', '08:00:00', '12:00:00', '13:00:00', '17:00:00', NULL, NULL, NULL, 1, 0);
-
-
+('2023-11-01', '08:01:00', '12:10:00', '13:00:00', '17:00:00', NULL, NULL, NULL, 1, 0),
+('2023-11-02', '08:04:00', '12:00:00', '13:00:00', '17:00:00', NULL, NULL, NULL, 1, 0),
+('2023-11-03', '08:00:00', '12:09:00', '13:01:00', '17:00:00', NULL, NULL, NULL, 1, 0),
+('2023-11-06', '08:00:00', '12:00:00', '13:00:00', '17:00:00', NULL, NULL, NULL, 1, 0),
+('2023-11-07', '08:06:00', '12:05:00', '13:00:00', '17:00:00', NULL, NULL, NULL, 1, 0),
+('2023-11-08', '08:00:00', '12:00:00', '13:00:00', '17:12:00', NULL, NULL, NULL, 1, 0),
+('2023-11-09', '08:00:00', '12:00:00', '13:54:00', '17:00:00', NULL, NULL, NULL, 1, 0),
+('2023-11-10', '08:02:00', '12:00:00', '13:01:00', '17:00:00', NULL, NULL, NULL, 1, 0),
+('2023-11-13', '08:01:00', '12:00:00', '13:30:00', '17:00:00', NULL, NULL, NULL, 1, 0),
+('2023-11-14', '08:00:00', '12:00:00', '13:10:00', '17:00:00', NULL, NULL, NULL, 1, 0),
+('2023-11-15', '08:00:00', '12:00:00', '13:00:00', '17:00:00', NULL, NULL, NULL, 1, 0),
+('2023-11-16', '08:00:00', '12:02:00', '13:00:00', '17:00:00', NULL, NULL, NULL, 1, 0),
+('2023-11-17', '08:04:00', '12:00:00', '13:00:00', '17:38:00', NULL, NULL, NULL, 1, 0),
+('2023-11-20', '08:08:00', '12:00:00', '13:00:00', '17:00:00', NULL, NULL, NULL, 1, 0),
+('2023-11-21', '08:10:00', '12:34:00', '13:00:00', '17:21:00', NULL, NULL, NULL, 1, 0),
+('2023-11-22', '08:00:00', '12:00:00', '13:00:00', '17:00:00', NULL, NULL, NULL, 1, 0),
+('2023-11-23', '08:05:00', '12:00:00', '13:00:00', '17:23:00', NULL, NULL, NULL, 1, 0),
+('2023-11-24', '08:00:00', '12:00:00', '13:00:00', '17:00:00', NULL, NULL, NULL, 1, 0),
+('2023-11-27', '08:06:00', '12:21:00', '13:05:00', '17:00:00', NULL, NULL, NULL, 1, 0),
+('2023-11-28', '08:00:00', '12:00:00', '13:33:00', '17:54:00', NULL, NULL, NULL, 1, 0);
